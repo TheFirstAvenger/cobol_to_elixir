4586c468445a08c14d05a21a17b08fbfe1db4149
TheFirstAvenger <mike@mikebinns.net>
2021-08-09 22:39:29 -0400

    Publish settings for 0.1.0 release

--- mix.exs
+++ mix.exs
@@ -17,7 +17,9 @@ defmodule CobolToElixir.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      source_url: "https://github.com/TheFirstAvenger/cobol_to_elixir",
+      package: package()
     ]
   end
 
@@ -36,7 +38,8 @@ defmodule CobolToElixir.MixProject do
   defp deps do
     [
       {:excoveralls, "~> 0.14.0", only: :test},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.25.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
@@ -45,6 +48,13 @@ defmodule CobolToElixir.MixProject do
   defp aliases do
     [
       coverage_report: [&coverage_report/1]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/TheFirstAvenger/cobol_to_elixir"}
     ]
   end
 
